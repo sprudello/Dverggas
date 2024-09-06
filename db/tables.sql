34a5c29a2bbcf59ccd4df55f84dd2758304a3a1b
Timog <110891995+timog06@users.noreply.github.com>
2024-09-06 17:23:10 +0200

    Update tables.sql
    Eine Grundstruktur für die Datenbank wurde erstellt.

---
+++ db/tables.sql
@@ -0,0 +1,34 @@
+-- Database
+CREATE DATABASE Dverggas;
+
+-- Tables
+USE Dverggas;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    display_name VARCHAR(32) NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    email_verified BOOLEAN DEFAULT 0,
+    phone_number VARCHAR(16) NULL UNIQUE,
+    phone_verified BOOLEAN DEFAULT 0,
+    street VARCHAR(255) NOT NULL,
+    street2 VARCHAR(255) NULL,
+    house_number VARCHAR(32) NOT NULL,
+    plz VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+USE Dverggas;
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(48) NOT NULL,
+    prod_desc VARCHAR(255) NULL,
+    price DECIMAL(12, 2) NULL,
+    brand VARCHAR(16) NOT NULL,
+    release_date TIMESTAMP NOT NULL,
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
