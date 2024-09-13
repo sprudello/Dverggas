e8fad31e06a27fe9b2469783c0010b8c0c41a5a3
Timog <110891995+timog06@users.noreply.github.com>
2024-09-13 15:43:46 +0200

    Update tables.sql
    Es waren noch ein paar kleine Fehler vorhanden und der Foreign Key war nicht richtig benutzt. Jetzt ist es gut.

--- db/tables.sql
+++ db/tables.sql
@@ -6,6 +6,8 @@ USE Dverggas;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
     display_name VARCHAR(32) NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     email_verified BOOLEAN DEFAULT 0,
@@ -33,11 +35,12 @@ CREATE TABLE categories (
 USE Dverggas;
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY (category_id) REFERENCES categories(id)
+    category_id INT,
     title VARCHAR(48) NOT NULL,
     prod_desc VARCHAR(255) NULL,
     price DECIMAL(12, 2) NULL,
     brand VARCHAR(16) NOT NULL,
     release_date TIMESTAMP NOT NULL,
     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
