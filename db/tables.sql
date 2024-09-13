c75bf4eb34401cb740482099bc0860ac5f72c4e6
Timog <110891995+timog06@users.noreply.github.com>
2024-09-13 17:15:19 +0200

    Update
    Kleine Fixes in den Dateien.

--- db/tables.sql
+++ db/tables.sql
@@ -1,11 +1,13 @@
--- Database
+-- Database ---
 CREATE DATABASE Dverggas;
 
--- Tables
+-- Tables ---
 USE Dverggas;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
     display_name VARCHAR(32) NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     email_verified BOOLEAN DEFAULT 0,
@@ -23,12 +25,22 @@ CREATE TABLE users (
 );
 
 USE Dverggas;
+CREATE TABLE categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    parent_id INT NULL,
+    FOREIGN KEY (parent_id) REFERENCES categories(id)
+);
+
+USE Dverggas;
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INT,
     title VARCHAR(48) NOT NULL,
     prod_desc VARCHAR(255) NULL,
     price DECIMAL(12, 2) NULL,
     brand VARCHAR(16) NOT NULL,
     release_date TIMESTAMP NOT NULL,
     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
--- include/head.php
+++ include/head.php
@@ -2,7 +2,7 @@
 <html lang="eng">
 <head>
     <title>Dverggas</title>
-    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" type="text/css" href="../styles/styles.css">
     <script src="scripts/script.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
