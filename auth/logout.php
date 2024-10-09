8d3aced8d3ffbb167b5306cac8644cce0f9cc0ce
Timog <110891995+timog06@users.noreply.github.com>
2024-10-09 14:56:54 +0100

    Revert "Updated Dverggas"
    This reverts commit b7da3927c5ed5699f2a4c32d0567eda1de1dddde.

--- auth/logout.php
+++ auth/logout.php
@@ -8,9 +8,6 @@ include_once '../include/head.php';
     <h2 class="login-title">Goodbye!</h2>
     <p>We wish you a happy day and hope to see you soon!</p>
     <a href="../index.php" class="back-home">Go to Homepage</a>
-    <script>
-    setTimeout(function() {
-        window.location.href = '../index.php';
-    }, 5000);
-    </script>
 </div>
+
+<?php include_once '../include/footer.php'; ?>
